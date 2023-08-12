5b36a66634064da2d3ea98ce9856f0268259ff2d
Kroos <Kroos@DESKTOP-QHKKA3F>
2023-08-12 13:24:25 +0500

    Implement the mehtod in GenericDataService

--- SimpleTrader.EntityFramework/Services/GenericDataService.cs
+++ SimpleTrader.EntityFramework/Services/GenericDataService.cs
@@ -1,4 +1,7 @@
-﻿using SimpleTrader.Domain.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace SimpleTrader.EntityFramework.Services
 {
-    public class GenericDataService : IDataService<T>
+    public class GenericDataService<T> : IDataService<T> where T : DomainObject
     {
         public readonly SimpleTraderDbContextFactory _contextFactory;
 
@@ -18,27 +21,58 @@ namespace SimpleTrader.EntityFramework.Services
 
         public async Task<T> Create(T entity)
         {
-            throw new NotImplementedException();
+            using (SimpleTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                EntityEntry<T> createdResult = await context.Set<T>().AddAsync(entity);
+                await context.SaveChangesAsync();
+
+                return createdResult.Entity;
+            }
         }
 
         public async Task<bool> Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SimpleTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+                context.Set<T>().Remove(entity);
+                await context.SaveChangesAsync();
+
+                return true;
+            }
         }
 
         public async Task<T> Get(int id)
         {
-            throw new NotImplementedException();
+            using (SimpleTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                T entity = await context.Set<T>().FirstOrDefaultAsync((e) => e.Id == id);
+
+                return entity;
+            }
         }
 
         public async Task<IEnumerable<T>> GetAll()
         {
-            throw new NotImplementedException();
+            using (SimpleTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                IEnumerable<T> entities = await context.Set<T>().ToListAsync();
+
+                return entities;
+            }
         }
 
         public async Task<T> Update(int id, T entity)
         {
-            throw new NotImplementedException();
+            using (SimpleTraderDbContext context = _contextFactory.CreateDbContext())
+            {
+                entity.Id = id;
+
+                context.Set<T>().Update(entity);
+                await context.SaveChangesAsync();
+
+                return entity;
+            }
         }
     }
 }
--- SimpleTrader.EntityFramework/SimpleTraderDbContextFactory.cs
+++ SimpleTrader.EntityFramework/SimpleTraderDbContextFactory.cs
@@ -10,7 +10,7 @@ namespace SimpleTrader.EntityFramework
 {
     public class SimpleTraderDbContextFactory : IDesignTimeDbContextFactory<SimpleTraderDbContext>
     {
-        public SimpleTraderDbContext CreateDbContext(string[] args)
+        public SimpleTraderDbContext CreateDbContext(string[] args = null)
         {
             var options = new DbContextOptionsBuilder<SimpleTraderDbContext>();
             options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SimpleTraderDB;Trusted_Connection=True;");
