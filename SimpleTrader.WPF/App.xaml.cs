173baa962625b0875611274fef3bad649479bc2b
Kroos <Kroos@DESKTOP-QHKKA3F>
2023-08-13 11:07:40 +0500

    Add viewModels and NAvigation Bar

--- SimpleTrader.WPF/App.xaml.cs
+++ SimpleTrader.WPF/App.xaml.cs
@@ -13,5 +13,12 @@ namespace SimpleTrader.WPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Window window = new MainWindow();
+            window.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
