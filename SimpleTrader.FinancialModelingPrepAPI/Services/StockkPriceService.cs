15459c1273d1c40028f0f6071a5474e19eff57c7
Kroos <Kroos@DESKTOP-QHKKA3F>
2023-08-25 20:24:43 +0500

    Add StockPriceService class to the SimpleTrader.FinancialModelingPrepAPI project and it inherit from IStockPriceService interface.

---
+++ SimpleTrader.FinancialModelingPrepAPI/Services/StockkPriceService.cs
@@ -0,0 +1,17 @@
+﻿using SimpleTrader.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.FinancialModelingPrepAPI.Services
+{
+    public class StockPriceService : IStockPriceService
+    {
+        public Task<double> GetPrice(string symbol)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
