94a9893986dce2332ee2d4d160488b03e7261f26
Kroos <Kroos@DESKTOP-QHKKA3F>
2023-08-26 18:52:05 +0500

    Add FinancilaModelingPrepHttpClient class for preventing duplicate code and implement it for each Service.

---
+++ SimpleTrader.FinancialModelingPrepAPI/FinancilaModelingPrepHttpClient.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.FinancialModelingPrepAPI
+{
+    public class FinancilaModelingPrepHttpClient: HttpClient
+    {
+        public FinancilaModelingPrepHttpClient()
+        {
+            this.BaseAddress = new Uri("https://financialmodelingprep.com/api/v3/");
+        }
+
+        public async Task<T> GetAsync<T>(string uri)
+        {
+            HttpResponseMessage response = await GetAsync(uri);
+            string jsonResponse = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<T>(jsonResponse);
+        }
+    }
+}
