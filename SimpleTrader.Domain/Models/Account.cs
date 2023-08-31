dedf0a0ff485c77c31bd21b2f989a3a57321fef8
Kroos <Kroos@DESKTOP-QHKKA3F>
2023-08-31 19:21:55 +0500

    Change the type of AssetTransactions property from IEnumrable to ICollection.

--- SimpleTrader.Domain/Models/Account.cs
+++ SimpleTrader.Domain/Models/Account.cs
@@ -10,6 +10,6 @@ namespace SimpleTrader.Domain.Models
     {
         public User AccountHolder { get; set; }
         public double Balance { get; set; }
-        public IEnumerable<AssetTransaction> AssetTransactions { get; set; }
+        public ICollection<AssetTransaction> AssetTransactions { get; set; }
     }
 }
