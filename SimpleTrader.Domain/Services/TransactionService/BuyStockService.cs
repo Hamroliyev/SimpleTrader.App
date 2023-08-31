ca093473e1d71b2d7c93d9b1f3f7a6ca75edf32c
Kroos <Kroos@DESKTOP-QHKKA3F>
2023-08-31 19:51:10 +0500

    Create IBuyStockService and BuyStickService class.

---
+++ SimpleTrader.Domain/Services/TransactionService/BuyStockService.cs
@@ -0,0 +1,53 @@
+﻿using SimpleTrader.Domain.Exceptions;
+using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Services.TransactionService
+{
+    public class BuyStockService : IBuyStuckService
+    {
+        private readonly IStockPriceService _stockPriceService;
+        private readonly IDataService<Account> _accountService;
+
+        public BuyStockService(IStockPriceService stockPriceService, IDataService<Account> dataService)
+        {
+            _stockPriceService = stockPriceService;
+            _accountService = dataService;
+        }
+
+        public async Task<Account> BuyStock(Account buyer, string symbol, int shares)
+        {
+            double stockPrice = await _stockPriceService.GetPrice(symbol);
+            double transactionPrice = stockPrice * shares;
+
+            if (transactionPrice>buyer.Balance)
+            {
+                throw new InsufficientFundsException(buyer.Balance, transactionPrice);
+            }
+
+            AssetTransaction asset = new AssetTransaction()
+            {
+                Account = buyer,
+                Asset = new Asset()
+                {
+                    PricePerShare = stockPrice,
+                    Symbol = symbol
+                },
+                DateProcessed = DateTime.Now,
+                Shares = shares,
+                IsPurchase = true
+            };
+
+            buyer.AssetTransactions.Add(asset);
+            buyer.Balance -= transactionPrice;
+
+            await _accountService.Update(buyer.Id, buyer);
+
+            return buyer;
+        }
+    }
+}
---
+++ SimpleTrader.Domain/Services/TransactionService/IBuyStuckService.cs
@@ -0,0 +1,14 @@
+﻿using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Services.TransactionService
+{
+    public interface IBuyStuckService
+    {
+        Task<Account> BuyStock(Account buyer, string symbol, int shares);
+    }
+}
