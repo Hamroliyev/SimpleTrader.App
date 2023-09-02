0e74c4979ed251a8077c43dd40e1d53e7504c765
Kroos <Kroos@DESKTOP-QHKKA3F>
2023-09-02 13:35:01 +0500

    Change MainViewModel for DI .

--- SimpleTrader.WPF/ViewModels/MainViewModel.cs
+++ SimpleTrader.WPF/ViewModels/MainViewModel.cs
@@ -9,9 +9,10 @@ namespace SimpleTrader.WPF.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
-        public INavigator Navigator { get; set; } = new Navigator();
-        public MainViewModel()
+        public INavigator Navigator { get; set; }
+        public MainViewModel(INavigator navigator)
         {
+            Navigator = navigator;
             Navigator.UpdateCurentViewModelCommand.Execute(ViewType.Home);
         }
     }
