84b5626d610f9a09f26389c703e14a64002f05cd
Kroos <Kroos@DESKTOP-QHKKA3F>
2023-09-02 14:28:37 +0500

    Update UpdateCurrentBiewModelCommane class.

--- SimpleTrader.WPF/Commands/UpdateCurrentViewModelCommand.cs
+++ SimpleTrader.WPF/Commands/UpdateCurrentViewModelCommand.cs
@@ -1,6 +1,7 @@
 ﻿using SimpleTrader.FinancialModelingPrepAPI.Services;
 using SimpleTrader.WPF.State.Navigators;
 using SimpleTrader.WPF.ViewModels;
+using SimpleTrader.WPF.ViewModels.Factories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,15 @@ namespace SimpleTrader.WPF.Commands
     public class UpdateCurrentViewModelCommand : ICommand
     {
         public event EventHandler CanExecuteChanged;
-        private INavigator _navigator;
 
-        public UpdateCurrentViewModelCommand(INavigator navigator)
+        private readonly INavigator _navigator;
+        private readonly ISimpleTraderViewModelAbstractFactory _viewModelFactory;
+
+        public UpdateCurrentViewModelCommand(INavigator navigator,
+            ISimpleTraderViewModelAbstractFactory viewModelFactory)
         {
             _navigator = navigator;
+            _viewModelFactory = viewModelFactory;
         }
 
         public bool CanExecute(object parameter)
@@ -31,17 +36,7 @@ namespace SimpleTrader.WPF.Commands
             {
                 ViewType viewType = (ViewType)parameter;
 
-                switch (viewType)
-                {
-                    case ViewType.Home:
-                        _navigator.CurrentViewModel = new HomeViewModel(MajorIndexListingViewModel.LoadMajorIndexViewModel(new MajorIndexService()));
-                        break;
-                    case ViewType.Portfolio:
-                        _navigator.CurrentViewModel = new PortfolioViewModel();
-                        break;
-                    default:
-                        break;
-                }
+                _navigator.CurrentViewModel = _viewModelFactory.CreateViewModel(viewType);
             }
         }
     }
