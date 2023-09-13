218c3b0ff538fb7154f6c6a73d34674b9c075da8
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-13 19:06:02 +0500

    Add Two command class for Buy view action.

---
+++ SimpleTrader.WPF/Commands/BuyStockCommand.cs
@@ -0,0 +1,49 @@
+﻿using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services.TransactionService;
+using SimpleTrader.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SimpleTrader.WPF.Commands
+{
+    public class BuyStockCommand : ICommand
+    {
+        private readonly BuyViewModel _buyViewModel;
+        private readonly IBuyStockService _buyStockService;
+
+        public BuyStockCommand(BuyViewModel buyViewModel, IBuyStockService buyStockService)
+        {
+            _buyViewModel = buyViewModel;
+            _buyStockService = buyStockService;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            try
+            {
+                Account account = await _buyStockService.BuyStock(new Account()
+                {
+                    Id = 1,
+                    Balance = 500,
+                    AssetTransactions = new List<AssetTransaction>()
+                }, _buyViewModel.Symbol, _buyViewModel.SharesToBuy);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
---
+++ SimpleTrader.WPF/Commands/SearchSymbolCommand.cs
@@ -0,0 +1,40 @@
+﻿using SimpleTrader.Domain.Services;
+using SimpleTrader.WPF.ViewModels;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SimpleTrader.WPF.Commands
+{
+    public class SearchSymbolCommand : ICommand
+    {
+        private readonly BuyViewModel _buyViewModel;
+        private readonly IStockPriceService _stockPriceService;
+
+        public SearchSymbolCommand(BuyViewModel buyViewModel, IStockPriceService stockPriceService)
+        {
+            _buyViewModel = buyViewModel;
+            _stockPriceService = stockPriceService;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            try
+            {
+                double stockPrice = await _stockPriceService.GetPrice(_buyViewModel.Symbol);
+                _buyViewModel.StockPrice = stockPrice;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+    }
+}
