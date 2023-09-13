4eac9e6060e8e18788ae10ca16ad841cc4f476cc
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-13 19:06:44 +0500

    Update RootSimpleTraderViewModelFactory class.

--- SimpleTrader.WPF/ViewModels/BuyViewModel.cs
+++ SimpleTrader.WPF/ViewModels/BuyViewModel.cs
@@ -1,7 +1,64 @@
-﻿namespace SimpleTrader.WPF.ViewModels
+﻿using SimpleTrader.Domain.Services;
+using SimpleTrader.Domain.Services.TransactionService;
+using SimpleTrader.WPF.Commands;
+using System.Windows.Input;
+
+namespace SimpleTrader.WPF.ViewModels
 {
     public class BuyViewModel : ViewModelBase
     {
+        private string _symbol;
 
+        public string Symbol
+        {
+            get { return _symbol; }
+            set 
+            { 
+                _symbol = value;
+                OnPropertyChanged(nameof(Symbol));
+            }
+        }
+        private double _stockPrice;
+
+        public double StockPrice
+        {
+            get { return _stockPrice; }
+            set 
+            { 
+                _stockPrice = value;
+                OnPropertyChanged(nameof(StockPrice));
+            }
+        }
+
+        private int _sharesToBuy;
+
+        public int SharesToBuy
+        {
+            get { return _sharesToBuy; }
+            set 
+            { 
+                _sharesToBuy = value;
+                OnPropertyChanged(nameof(SharesToBuy));
+                OnPropertyChanged(nameof(TotalPrice));
+            }
+        }
+
+        public double TotalPrice
+        {
+            get
+            {
+                return SharesToBuy * StockPrice;
+            }
+        }
+
+
+        public ICommand SearchSymbolCommand { get; set; }
+        public ICommand BuyStockCommand { get; set; }
+
+        public BuyViewModel(IStockPriceService stockPriceService, IBuyStockService buyStockService)
+        {
+            SearchSymbolCommand = new SearchSymbolCommand(this, stockPriceService);
+            BuyStockCommand = new BuyStockCommand(this, buyStockService);
+        }
     }
 }
--- SimpleTrader.WPF/ViewModels/Factories/RootSimpleTraderViewModelFactory.cs
+++ SimpleTrader.WPF/ViewModels/Factories/RootSimpleTraderViewModelFactory.cs
@@ -1,9 +1,5 @@
 ﻿using SimpleTrader.WPF.State.Navigators;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleTrader.WPF.ViewModels.Factories
 {
