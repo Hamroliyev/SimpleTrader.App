64b43cfe923023f1330b7caee4ca4ba0dac0e905
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-15 17:20:02 +0500

    Rename Passowrd to PasswordHash and update database.

--- SimpleTrader.Domain/Models/User.cs
+++ SimpleTrader.Domain/Models/User.cs
@@ -11,6 +11,6 @@ namespace SimpleTrader.Domain.Models
         public string Email { get; set; }
         public string Username { get; set; }
         public DateTime DateJoined { get; set; }
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
     }
 }
--- SimpleTrader.EntityFramework/Migrations/20230915121610_password-hash.Designer.cs
+++ SimpleTrader.EntityFramework/Migrations/20230915121610_password-hash.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SimpleTrader.EntityFramework;
 
 namespace SimpleTrader.EntityFramework.Migrations
 {
     [DbContext(typeof(SimpleTraderDbContext))]
-    partial class SimpleTraderDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230915121610_password-hash")]
+    partial class passwordhash
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
