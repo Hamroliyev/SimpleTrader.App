b54e038da9d8170e7aad5750147b6a5dfc766bdd
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-16 17:26:28 +0500

    Add UserNotFoundException custom exception.

---
+++ SimpleTrader.Domain/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Exceptions
+{
+    public class UserNotFoundException : Exception
+    {
+        public string UserName { get; set; }
+        public UserNotFoundException(string userName)
+        {
+            UserName = userName;
+        }
+
+        public UserNotFoundException(string message, string userName) : base(message)
+        {
+            UserName = userName;
+        }
+
+        public UserNotFoundException(string message, Exception innerException, string userName) : base(message, innerException)
+        {
+            UserName = userName;
+        }
+    }
+}
