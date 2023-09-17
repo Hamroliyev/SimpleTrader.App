b170495cbe4a91086c79407740d7649ce5fe190c
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-17 18:13:13 +0500

    Add IAuthenticator interface and implement it.

---
+++ SimpleTrader.WPF/State/Authenticators/Authenticator.cs
@@ -0,0 +1,49 @@
+﻿using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services.AuthenticationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.State.Authenticators
+{
+    public class Authenticator : IAuthenticator
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public Authenticator(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        public Account CurrentAccount { get; private set; }
+
+        public bool IsLoggedIn => CurrentAccount != null;
+
+        public async Task<bool> Login(string userName, string password)
+        {
+            bool success = true;
+            try
+            {
+                CurrentAccount = await _authenticationService.Login(userName, password);
+            }
+            catch (Exception)
+            {
+                success = false;
+            }
+
+            return success;
+        }
+
+        public void LogOut()
+        {
+            CurrentAccount = null;
+        }
+
+        public async Task<RegistrationResult> Register(string email, string userName, string password, string confirmPassword)
+        {
+            return await _authenticationService.Register(email, userName, password, confirmPassword);
+        }
+    }
+}
---
+++ SimpleTrader.WPF/State/Authenticators/IAuthenticator.cs
@@ -0,0 +1,20 @@
+﻿using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services.AuthenticationServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.State.Authenticators
+{
+    public interface IAuthenticator
+    {
+        Account CurrentAccount { get; }
+        bool IsLoggedIn { get; }
+        Task<RegistrationResult> Register(string email, string userName, string password, string confirmPassword);
+        Task<bool> Login(string userName, string password);
+        void LogOut();
+
+    }
+}
