0a9279adcc8e7bb796d0eb63994cdfe76dbfa980
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-19 18:42:36 +0500

    Refactor LoginViewModelFactory class.

--- SimpleTrader.WPF/ViewModels/Factories/LoginViewModelFactory.cs
+++ SimpleTrader.WPF/ViewModels/Factories/LoginViewModelFactory.cs
@@ -1,4 +1,5 @@
 ﻿using SimpleTrader.WPF.State.Authenticators;
+using SimpleTrader.WPF.State.Navigators;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +11,17 @@ namespace SimpleTrader.WPF.ViewModels.Factories
     public class LoginViewModelFactory : ISimpleTraderViewModelFactory<LoginViewModel>
     {
         private readonly IAuthenticator _authenticator;
+        private readonly INavigator _navigator;
 
-        public LoginViewModelFactory(IAuthenticator authenticator)
+        public LoginViewModelFactory(IAuthenticator authenticator, INavigator navigator)
         {
             _authenticator = authenticator;
+            _navigator = navigator;
         }
 
         public LoginViewModel CreateViewModel()
         {
-            return new LoginViewModel(_authenticator);
+            return new LoginViewModel(_authenticator, _navigator);
         }
     }
 }
