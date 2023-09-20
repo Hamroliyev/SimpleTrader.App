0f67aa730fd2e034368b5237a154f9e5252d1c96
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-20 17:32:38 +0500

    Add UpdateCurentViewModelCommand to MainViewModel as a field.

--- SimpleTrader.WPF/ViewModels/MainViewModel.cs
+++ SimpleTrader.WPF/ViewModels/MainViewModel.cs
@@ -1,23 +1,32 @@
-﻿using SimpleTrader.WPF.State.Authenticators;
+﻿using SimpleTrader.WPF.Commands;
+using SimpleTrader.WPF.State.Authenticators;
 using SimpleTrader.WPF.State.Navigators;
+using SimpleTrader.WPF.ViewModels.Factories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SimpleTrader.WPF.ViewModels
 {
     public class MainViewModel : ViewModelBase
     {
+        private readonly IRootSimpleTraderViewModelFactory _viewModelFactory;
+
         public INavigator Navigator { get; set; }
         public IAuthenticator Authenticator { get; set; }
+        public ICommand UpdateCurentViewModelCommand { get; }
 
-        public MainViewModel(INavigator navigator, IAuthenticator authenticator)
+        public MainViewModel(INavigator navigator,IRootSimpleTraderViewModelFactory viewModelFactory, IAuthenticator authenticator)
         {
             Navigator = navigator;
+            _viewModelFactory = viewModelFactory;
             Authenticator = authenticator;
-            Navigator.UpdateCurentViewModelCommand.Execute(ViewType.Login);
+
+            UpdateCurentViewModelCommand = new UpdateCurrentViewModelCommand(navigator, _viewModelFactory);
+            UpdateCurentViewModelCommand.Execute(ViewType.Login);
         }
     }
 }
