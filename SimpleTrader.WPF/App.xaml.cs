4385ec804fdc0817d65d9edc9a215c09566e1472
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-20 18:00:10 +0500

    Register IRenavigator to the DI.

--- SimpleTrader.WPF/App.xaml.cs
+++ SimpleTrader.WPF/App.xaml.cs
@@ -57,7 +57,11 @@ namespace SimpleTrader.WPF
             services.AddSingleton<ISimpleTraderViewModelFactory<HomeViewModel>, HomeViewModelFactory>();
             services.AddSingleton<ISimpleTraderViewModelFactory<PortfolioViewModel>, PortfolioViewModelFactory>();
             services.AddSingleton<ISimpleTraderViewModelFactory<MajorIndexListingViewModel>, MajorIndexListingViewModelFactory>();
-            services.AddSingleton<ISimpleTraderViewModelFactory<LoginViewModel>, LoginViewModelFactory>();
+
+            services.AddSingleton<ISimpleTraderViewModelFactory<LoginViewModel>>((services) =>
+                new LoginViewModelFactory(services.GetRequiredService<IAuthenticator>(),
+                new ViewModelFactoryRenavigator<HomeViewModel>(services.GetRequiredService<INavigator>(),
+                services.GetRequiredService<ISimpleTraderViewModelFactory<HomeViewModel>>())));
 
             services.AddScoped<INavigator, Navigator>();
             services.AddScoped<IAuthenticator, Authenticator>();
--- SimpleTrader.WPF/State/Navigators/ViewModelFactoryRenavigator.cs
+++ SimpleTrader.WPF/State/Navigators/ViewModelFactoryRenavigator.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace SimpleTrader.WPF.State.Navigators
 {
-    public class Renavigator<TViewModel> : IRenavigator where TViewModel : ViewModelBase
+    public class ViewModelFactoryRenavigator<TViewModel> : IRenavigator where TViewModel : ViewModelBase
     {
         private readonly INavigator _navigator;
         private readonly ISimpleTraderViewModelFactory<TViewModel> _viewModelFactory;
 
-        public Renavigator(INavigator navigator, ISimpleTraderViewModelFactory<TViewModel> viewModelFactory)
+        public ViewModelFactoryRenavigator(INavigator navigator, ISimpleTraderViewModelFactory<TViewModel> viewModelFactory)
         {
             _navigator = navigator;
             _viewModelFactory = viewModelFactory;
