05e796c0ec06014a939f46826b148f31d2259189
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-20 17:44:56 +0500

    Create IRenavigator and implent it.

---
+++ SimpleTrader.WPF/State/Navigators/IRenavigator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.State.Navigators
+{
+    public interface IRenavigator
+    {
+        void Renavigate();
+    }
+}
---
+++ SimpleTrader.WPF/State/Navigators/Renavigator.cs
@@ -0,0 +1,27 @@
+﻿using SimpleTrader.WPF.ViewModels;
+using SimpleTrader.WPF.ViewModels.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.State.Navigators
+{
+    public class Renavigator<TViewModel> : IRenavigator where TViewModel : ViewModelBase
+    {
+        private readonly INavigator _navigator;
+        private readonly ISimpleTraderViewModelFactory<TViewModel> _viewModelFactory;
+
+        public Renavigator(INavigator navigator, ISimpleTraderViewModelFactory<TViewModel> viewModelFactory)
+        {
+            _navigator = navigator;
+            _viewModelFactory = viewModelFactory;
+        }
+
+        public void Renavigate()
+        {
+            _navigator.CurrentViewModel = _viewModelFactory.CreateViewModel();
+        }
+    }
+}
