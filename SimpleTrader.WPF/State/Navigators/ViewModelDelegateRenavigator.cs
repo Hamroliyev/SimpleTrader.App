40df46f958f043e8f341fc04c415cc76d19a0532
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-21 16:49:47 +0500

    Rename ViewModelDelegateRenavigator  and refactor it.

---
+++ SimpleTrader.WPF/State/Navigators/ViewModelDelegateRenavigator.cs
@@ -0,0 +1,27 @@
+﻿using SimpleTrader.WPF.ViewModels;
+using SimpleTrader.WPF.ViewModels.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.State.Navigators
+{
+    public class ViewModelDelegateRenavigator<TViewModel> : IRenavigator where TViewModel : ViewModelBase
+    {
+        private readonly INavigator _navigator;
+        private readonly CreateViewModel<TViewModel> _createViewModel;
+
+        public ViewModelDelegateRenavigator(INavigator navigator, CreateViewModel<TViewModel> createViewModel)
+        {
+            _navigator = navigator;
+            _createViewModel = createViewModel;
+        }
+
+        public void Renavigate()
+        {
+            _navigator.CurrentViewModel = _createViewModel();
+        }
+    }
+}
