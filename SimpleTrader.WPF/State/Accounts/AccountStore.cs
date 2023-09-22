bd81922bd1c2da9f029346421e53860d2857bff1
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-22 19:25:54 +0500

    Add IAccountStore and AccountStore to the project implement it.

---
+++ SimpleTrader.WPF/State/Accounts/AccountStore.cs
@@ -0,0 +1,14 @@
+﻿using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.State.Accounts
+{
+    public class AccountStore : IAccountStore
+    {
+        public Account CurrentAccount { get; set; }
+    }
+}
---
+++ SimpleTrader.WPF/State/Accounts/IAccountStore.cs
@@ -0,0 +1,14 @@
+﻿using SimpleTrader.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.State.Accounts
+{
+    public interface IAccountStore
+    {
+        Account CurrentAccount { get;set }
+    }
+}
