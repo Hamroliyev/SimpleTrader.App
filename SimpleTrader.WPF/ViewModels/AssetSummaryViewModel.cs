af21b4831eb40eb7ebdc33a8e69ad8d4cb849d1a
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-25 19:15:14 +0500

    Inherit The ViewModel from ViewModelBase.

--- SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
+++ SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SimpleTrader.WPF.ViewModels
 {
-    public class AssetSummaryViewModel
+    public class AssetSummaryViewModel : ViewModelBase
     {
 
     }
