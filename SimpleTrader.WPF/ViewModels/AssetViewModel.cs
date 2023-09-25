91fcf4a02eafd4e5f3057910f7ecdc051e737387
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-25 19:32:35 +0500

    Dove Code : AssetViewModel.

---
+++ SimpleTrader.WPF/ViewModels/AssetViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.WPF.ViewModels
+{
+    public class AssetViewModel
+    {
+        public string Symbol { get; }
+        public int Shares { get; }
+        public AssetViewModel(string symbol, int shares)
+        {
+            Symbol = symbol;
+            Shares = shares;
+        }
+
+    }
+}
