bd439979fe4c5f46c44a2cd201921fa0198e78b7
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-25 18:48:02 +0500

    Add StateChanged event to IAccountStore and implement it in AccountStore

--- SimpleTrader.WPF/State/Accounts/AccountStore.cs
+++ SimpleTrader.WPF/State/Accounts/AccountStore.cs
@@ -9,6 +9,20 @@ namespace SimpleTrader.WPF.State.Accounts
 {
     public class AccountStore : IAccountStore
     {
-        public Account CurrentAccount { get; set; }
+        private Account _currentAccount;
+
+        public Account CurrentAccount
+        {
+            get { return _currentAccount; }
+            set
+            {
+                _currentAccount = value;
+                StateChanged?.Invoke();
+            }
+        }
+
+
+
+        public event Action StateChanged;
     }
 }
--- SimpleTrader.WPF/State/Accounts/IAccountStore.cs
+++ SimpleTrader.WPF/State/Accounts/IAccountStore.cs
@@ -1,14 +1,11 @@
 ﻿using SimpleTrader.Domain.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SimpleTrader.WPF.State.Accounts
 {
     public interface IAccountStore
     {
         Account CurrentAccount { get; set; }
+        event Action StateChanged; 
     }
 }
