0c1c4d0fe2b6aa53b0736daec11991c948791529
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-09-26 17:36:15 +0500

    AssetSummaryViewModel : Dove code in the view model and add AssetStore and Collection of AssetViewModel.

--- SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
+++ SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SimpleTrader.WPF.State.Assets;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,27 @@ namespace SimpleTrader.WPF.ViewModels
 {
     public class AssetSummaryViewModel : ViewModelBase
     {
+        private readonly AssetStore _assetStore;
+        private readonly ObservableCollection<AssetViewModel> _assets;
 
+        public double AccountBalance => _assetStore.AccountBalance;
+        public IEnumerable<AssetViewModel> Assets => _assets;
+        public AssetSummaryViewModel(AssetStore assetStore)
+        {
+            _assetStore = assetStore;
+            _assets = new ObservableCollection<AssetViewModel>();
+            _assetStore.StateChanged += AssetStore_StateChanged;
+        }
+
+        private void AssetStore_StateChanged()
+        {
+            OnPropertyChanged(nameof(AccountBalance));
+            ResetAssets();
+        }
+
+        private void ResetAssets()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
