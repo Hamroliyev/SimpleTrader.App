1aa36247e8c92c878c50ce01a68ca3c2c797a199
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-03 18:42:03 +0500

    Remove unnececcery using statements.

--- SimpleTrader.WPF/ViewModels/LoginViewModel.cs
+++ SimpleTrader.WPF/ViewModels/LoginViewModel.cs
@@ -1,11 +1,6 @@
 ﻿using SimpleTrader.WPF.Commands;
 using SimpleTrader.WPF.State.Authenticators;
 using SimpleTrader.WPF.State.Navigators;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace SimpleTrader.WPF.ViewModels
