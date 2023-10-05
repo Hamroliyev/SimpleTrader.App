b2a66fb54d5e44f096dca0c7f48a4a7f770e3265
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-05 17:37:21 +0500

    Documentate the IStockPriceService'  getPrice method.

--- SimpleTrader.Domain/Services/IStockPriceService.cs
+++ SimpleTrader.Domain/Services/IStockPriceService.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SimpleTrader.Domain.Exceptions;
 using System.Threading.Tasks;
 
 namespace SimpleTrader.Domain.Services
 {
     public interface IStockPriceService
     {
+        /// <summary>
+        /// Get the share price for a symbol.
+        /// </summary>
+        /// <param name="symbol"></param>
+        /// <returns>The price of symbol.</returns>
+        /// <exception cref="InvalidSymbolException">Thrown if symbol does not exist.</exception>
         Task<double> GetPrice(string symbol);
     }
 }
