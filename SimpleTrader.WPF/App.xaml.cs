58ae27115531416c49e4f79819d7169865f04dc2
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-06 15:49:10 +0500

    App.xaml: Add defoult OnExit mehtod.

--- SimpleTrader.WPF/App.xaml.cs
+++ SimpleTrader.WPF/App.xaml.cs
@@ -95,10 +95,18 @@ namespace SimpleTrader.WPF
         }
         protected override void OnStartup(StartupEventArgs e)
         {
+            _host.Start();
             Window window =  _host.Services.GetRequiredService<MainWindow>();
             window.Show();
 
             base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+            base.OnExit(e);
         }
     }
 }
