c17a5266304c9ef3f31229cc4746380b109abe1b
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-07 19:16:18 +0500

    Commands: Add RenavigateCommand.

---
+++ SimpleTrader.WPF/Commands/RenavigateCommand.cs
@@ -0,0 +1,32 @@
+﻿using SimpleTrader.WPF.State.Navigators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SimpleTrader.WPF.Commands
+{
+    public class RenavigateCommand : ICommand
+    {
+        private readonly IRenavigator _renavigator;
+
+        public RenavigateCommand(IRenavigator renavigator)
+        {
+            _renavigator = renavigator;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _renavigator.Renavigate();
+        }
+    }
+}
