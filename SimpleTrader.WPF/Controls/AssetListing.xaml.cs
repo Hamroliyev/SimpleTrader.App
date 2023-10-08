f3326e66de82388d64b55b22416fabfbfe48ebff
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-08 16:32:41 +0500

    Add AssetListing user control and update other views.

---
+++ SimpleTrader.WPF/Controls/AssetListing.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimpleTrader.WPF.Controls
+{
+    /// <summary>
+    /// Interaction logic for AssetListing.xaml
+    /// </summary>
+    public partial class AssetListing : UserControl
+    {
+        public AssetListing()
+        {
+            InitializeComponent();
+        }
+    }
+}
