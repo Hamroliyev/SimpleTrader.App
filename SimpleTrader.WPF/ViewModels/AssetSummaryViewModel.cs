8a984738cff80d6394868b71fc958d6ae3e25918
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-08 17:12:20 +0500

    Update viewmodels with AseetListingViewModel.

--- SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
+++ SimpleTrader.WPF/ViewModels/AssetSummaryViewModel.cs
@@ -8,35 +8,21 @@ namespace SimpleTrader.WPF.ViewModels
     public class AssetSummaryViewModel : ViewModelBase
     {
         private readonly AssetStore _assetStore;
-        private readonly ObservableCollection<AssetViewModel> _topAssets;
 
         public double AccountBalance => _assetStore.AccountBalance;
-        public IEnumerable<AssetViewModel> TopAssets => _topAssets;
+        public AssetListingViewModel AssetListingViewModel { get; }
+
         public AssetSummaryViewModel(AssetStore assetStore)
         {
             _assetStore = assetStore;
-            _topAssets = new ObservableCollection<AssetViewModel>();
-            _assetStore.StateChanged += AssetStore_StateChanged;
-            ResetAssets();
-        }
-        private void ResetAssets()
-        {
-            IEnumerable<AssetViewModel> assetViewModels = _assetStore.AssetTransactions
-                .GroupBy(g => g.Asset.Symbol)
-                .Select(g => new AssetViewModel(g.Key, g.Sum(a => a.IsPurchase ? a.Shares : -a.Shares)))
-                .Where(a => a.Shares > 0)
-                .Take(3);
+            AssetListingViewModel = new AssetListingViewModel(assetStore, assets => assets.Take(3));
 
-            _topAssets.Clear();
-            foreach (AssetViewModel assetViewModel in assetViewModels)
-            {
-                _topAssets.Add(assetViewModel);
-            }
+            _assetStore.StateChanged += AssetStore_StateChanged;
         }
+
         private void AssetStore_StateChanged()
         {
             OnPropertyChanged(nameof(AccountBalance));
-            ResetAssets();
         }
 
     }
--- SimpleTrader.WPF/ViewModels/PortfolioViewModel.cs
+++ SimpleTrader.WPF/ViewModels/PortfolioViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SimpleTrader.WPF.State.Assets;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace SimpleTrader.WPF.ViewModels
 {
     public class PortfolioViewModel : ViewModelBase
     {
+        public AssetListingViewModel AssetListingViewModel { get; }
+
+        public PortfolioViewModel(AssetStore assetStore)
+        {
+            AssetListingViewModel = new AssetListingViewModel(assetStore);
+        }
     }
 }
