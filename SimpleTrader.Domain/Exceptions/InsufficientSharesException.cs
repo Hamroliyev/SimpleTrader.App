9b48d3273ef15751d27e45f909527562ee5d0201
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-10 18:28:30 +0500

    Add custom InsufficientSharesException exception.

---
+++ SimpleTrader.Domain/Exceptions/InsufficientSharesException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Exceptions
+{
+    public class InsufficientSharesException : Exception
+    {
+        public string Symbol { get; }
+        public int AccountShares { get; }
+        public int RequiredShares { get; }
+
+        public InsufficientSharesException(string symbol, int accountShares, int requiredShares)
+        {
+            Symbol = symbol;
+            AccountShares = accountShares;
+            RequiredShares = requiredShares;
+        }
+
+        public InsufficientSharesException(string message, string symbol, int accountShares, int requiredShares) : base(message)
+        {
+            Symbol = symbol;
+            AccountShares = accountShares;
+            RequiredShares = requiredShares;
+        }
+
+        public InsufficientSharesException(string message, Exception innerException, string symbol, int accountShares, int requiredShares) : base(message, innerException)
+        {
+            Symbol = symbol;
+            AccountShares = accountShares;
+            RequiredShares = requiredShares;
+        }
+    }
+}
