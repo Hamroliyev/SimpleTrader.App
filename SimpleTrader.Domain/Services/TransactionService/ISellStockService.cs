8a874795467cecbbcc4f273965431c868b1c6ac2
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-10 18:25:22 +0500

    Add ISellStockService and its implementation.

---
+++ SimpleTrader.Domain/Services/TransactionService/ISellStockService.cs
@@ -0,0 +1,22 @@
+﻿using SimpleTrader.Domain.Models;
+using System;
+using SimpleTrader.Domain.Exceptions;
+using System.Threading.Tasks;
+
+namespace SimpleTrader.Domain.Services.TransactionService
+{
+    public interface ISellStockService
+    {
+        /// <summary>
+        /// Sell a stock for an account.
+        /// </summary>
+        /// <param name="seller">The account of the seller.</param>
+        /// <param name="symbol">The symbol sold.</param>
+        /// <param name="shares">The amount of shares to sell.</param>
+        /// <returns>The updated account.</returns>
+        /// <exception cref="InsufficientSharesException">Thrown if the seller has insufficient shares for the symbol.</exception>
+        /// <exception cref="InvalidSymbolException">Thrown if the purchased symbol is invalid.</exception>
+        /// <exception cref="Exception">Thrown if the transaction fails.</exception>
+        Task<Account> SellStock(Account seller, string symbol, int shares);
+    }
+}
