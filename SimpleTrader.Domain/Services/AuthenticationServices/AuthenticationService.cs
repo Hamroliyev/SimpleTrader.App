a942d9e5bec906e8087f6ab35ac561792318254a
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-11 13:30:01 +0500

    Clean up the services..

--- SimpleTrader.Domain/Services/AuthenticationServices/AuthenticationService.cs
+++ SimpleTrader.Domain/Services/AuthenticationServices/AuthenticationService.cs
@@ -2,9 +2,6 @@
 using SimpleTrader.Domain.Exceptions;
 using SimpleTrader.Domain.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleTrader.Domain.Services.AuthenticationServices
