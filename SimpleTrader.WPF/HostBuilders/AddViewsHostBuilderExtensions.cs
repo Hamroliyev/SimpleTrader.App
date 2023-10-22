612c4b8cedcb1edfee629c9908fe3cc4b6081480
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-22 18:53:05 +0500

    AddViewsHostBuilderExtensions

---
+++ SimpleTrader.WPF/HostBuilders/AddViewsHostBuilderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SimpleTrader.WPF.ViewModels;
+
+namespace SimpleTrader.WPF.HostBuilders
+{
+    public static class AddViewsHostBuilderExtensions
+    {
+        public static IHostBuilder AddViews(this IHostBuilder host)
+        {
+            host.ConfigureServices(services =>
+            {
+                services.AddSingleton<MainWindow>(s => new MainWindow(s.GetRequiredService<MainViewModel>()));
+            });
+
+            return host;
+        }
+    }
+}
