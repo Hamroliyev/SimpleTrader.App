0b755645a4d526e23f65be8bd986ef43838fc04c
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-22 18:48:51 +0500

    Create AddServicesHostBuilderExtensions extensionmethod.

---
+++ SimpleTrader.WPF/HostBuilders/AddServicesHostBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SimpleTrader.Domain.Models;
+using SimpleTrader.Domain.Services;
+using SimpleTrader.Domain.Services.AuthenticationServices;
+using SimpleTrader.Domain.Services.TransactionService;
+using SimpleTrader.EntityFramework.Services;
+using SimpleTrader.FinancialModelingPrepAPI.Services;
+
+namespace SimpleTrader.WPF.HostBuilders
+{
+    public static class AddServicesHostBuilderExtensions
+    {
+        public static IHostBuilder AddServices(this IHostBuilder host)
+        {
+            host.ConfigureServices(services =>
+            {
+                services.AddSingleton<IPasswordHasher, PasswordHasher>();
+
+                services.AddSingleton<IAuthenticationService, AuthenticationService>();
+                services.AddSingleton<IDataService<Account>, AccountDataService>();
+                services.AddSingleton<IAccountService, AccountDataService>();
+                services.AddSingleton<IStockPriceService, StockPriceService>();
+                services.AddSingleton<IBuyStockService, BuyStockService>();
+                services.AddSingleton<ISellStockService, SellStockService>();
+                services.AddSingleton<IMajorIndexService, MajorIndexService>();
+            });
+
+            return host;
+        }
+    }
+}
