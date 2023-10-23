4c1b3e94dd9b3120f40339ac194a031c3a4e52a2
Kroos <hamroliyevahmadjon1909@gmail.com>
2023-10-23 13:07:49 +0500

    Remove unnneccery using and format document.

--- SimpleTrader.FinancialModelingPrepAPI/Services/MajorIndexService.cs
+++ SimpleTrader.FinancialModelingPrepAPI/Services/MajorIndexService.cs
@@ -1,34 +1,24 @@
-﻿using Newtonsoft.Json;
-using SimpleTrader.Domain.Models;
+﻿using SimpleTrader.Domain.Models;
 using SimpleTrader.Domain.Services;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SimpleTrader.FinancialModelingPrepAPI.Services
 {
     public class MajorIndexService : IMajorIndexService
     {
-        private readonly FinancialModelingPrepHttpClientFactory httpClientFactory;
+        private readonly FinancialModelingPrepHttpClient httpClient
 
-        public MajorIndexService(FinancialModelingPrepHttpClientFactory httpClientFactory)
+        public MajorIndexService(FinancialModelingPrepHttpClient httpClientFactory)
         {
-            this.httpClientFactory = httpClientFactory;
+            this.httpClient = httpClientFactory;
         }
         public async Task<MajorIndex> GetMajorIndex(MajorIndexType indexType)
         {
-            using (FinancialModelingPrepHttpClient client = this.httpClientFactory.CreateHttpClient())
-            {
-                string uri = "major-indexes/" + GetUriSuffix(indexType);
-
-
-                MajorIndex majorIndex = await client.GetAsync<MajorIndex>(uri);
-                majorIndex.Type = indexType;
-                return majorIndex;
-            }
+            string uri = "major-indexes/" + GetUriSuffix(indexType);
+            MajorIndex majorIndex = await this.httpClient.GetAsync<MajorIndex>(uri);
+            majorIndex.Type = indexType;
+            return majorIndex;
         }
 
         private string GetUriSuffix(MajorIndexType indexType)
